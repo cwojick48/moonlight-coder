e7b4599ef3f9e12cde9a6a20776777b01fc9b656
cwojick48 <57726669+cwojick48@users.noreply.github.com>
2022-12-05 21:55:09 -0500

    Added initial survey page and html/css updates
    This commit adds an initial pass at a user survey page and updates
    css/html formatting

---
+++ moonlight_coder/survey.py
@@ -0,0 +1,17 @@
+
+class UserSurvey:
+    def __init__(self):
+        self.score = None
+        self.list_of_questions = []
+
+    def load_survey(self):
+        pass
+
+    def check_answer(self):
+        pass
+
+    def get_average(self):
+        """
+        Method to get the average of user surveyed answers 
+        """
+        pass
--- moonlight_coder/views.py
+++ moonlight_coder/views.py
@@ -218,6 +218,12 @@ def submit_answer(module):
     print(f"using {response=}")
     return redirect(url_for('flash_cards', module=module, response=response))
 
+@app.route('/survey')
+@login_required
+def survey():
+    print(f"questions for quiz loading...")
+
+    return render_template('main.html', file='survey.html')
 
 # this route is just for testing the database functions, not for production
 @app.route('/db')
